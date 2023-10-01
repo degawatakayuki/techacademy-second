4292078d2df1a3f217a9b0e321c48cf913fbb1b1
degawatakayuki <>
2023-10-01 23:15:47 +0900

    初期ファイル

---
+++ JS/bamburger.js
@@ -0,0 +1,17 @@
+// let menu_btn = document.getElementById('hamburger-btn');
+// let header_nav = document.getElementById('headerNavi');
+// let farstViewBg = document.getElementById('farstView');
+
+// menu_btn.addEventListener('click' , function () {
+//     header_nav.classList.toggle('menu-open');
+//     farstViewBg.classList.toggle('menu-open');
+// } );
+
+let menu_btn = document.getElementById('navToggle');
+let header_nav = document.getElementById('headerNavi');
+let farstViewBg = document.getElementById('farstView');
+
+menu_btn.addEventListener('click' , function () {
+    // header_nav.classList.toggle('menu-open');
+    farstViewBg.classList.toggle('menu-open');
+} );
